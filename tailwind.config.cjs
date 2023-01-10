02abe5af544e2bdf17c7712d8f0d6e4bb5e8b0fa
Mats Andresen <mats.andresen@gmx.de>
2023-01-10 18:59:48 +0100

    Setup fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      sansSerif: ['Source Sans Pro', 'sans-serif'],
+      mono: ['Source Code Pro', 'monospace'],
+      handwriting: ['Kalam', 'cursive']
+    },
     extend: {}
   },
   plugins: []
