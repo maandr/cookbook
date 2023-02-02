3f024d008cd155b38dc0d475ee51f248b3da817e
Mats Andresen <mats.andresen@gmx.de>
2023-02-02 15:03:52 +0100

    Actually log some errors

--- src/lib/server/database/PostgresClient.ts
+++ src/lib/server/database/PostgresClient.ts
@@ -1,20 +1,35 @@
 import pg from 'pg'
-import type { QueryResult, Pool } from 'pg'
+import type { QueryResult, Pool, PoolClient } from 'pg'
 
 export class PostgresClient {
   static async create(connectionString: string): Promise<PostgresClient> {
     const client = new PostgresClient(connectionString)
-    await client.connect().catch((e: unknown) => console.error('failed to connect to database', e))
+    await client.connect()
     return Promise.resolve(client)
   }
 
   private constructor(
     connectionString: string,
     private pool: Pool = new pg.Pool({ connectionString })
-  ) {}
+  ) {
+    this.configurePool()
+  }
+
+  private configurePool() {
+    this.pool.on('error', (error: Error) => {
+      console.error(error)
+    })
+    this.pool.on('acquire', (client: PoolClient) => {
+      client.on('error', (e: unknown) => {
+        console.error(e)
+      })
+    })
+  }
 
   private async connect() {
-    await this.pool.connect()
+    await this.pool
+      .connect()
+      .catch((e: Error) => console.error('failed to connect with database', e))
   }
 
   async query(statement: string, values: string[] = []): Promise<QueryResult> {
