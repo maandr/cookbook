fab729a208ae3cf9fefab341b6c0abfd3dfa6a5a
Mats Andresen <mats.andresen@gmx.de>
2023-02-02 14:34:53 +0100

    Add some debug logging

--- src/lib/server/database/PostgresClient.ts
+++ src/lib/server/database/PostgresClient.ts
@@ -4,12 +4,12 @@ import type { QueryResult, Pool } from 'pg'
 export class PostgresClient {
   static async create(connectionString: string): Promise<PostgresClient> {
     const client = new PostgresClient(connectionString)
-    await client.connect()
+    await client.connect().catch((e: unknown) => console.error('failed to connect to database', e))
     return Promise.resolve(client)
   }
 
   private constructor(
-    private connectionString: string,
+    connectionString: string,
     private pool: Pool = new pg.Pool({ connectionString })
   ) {}
 
