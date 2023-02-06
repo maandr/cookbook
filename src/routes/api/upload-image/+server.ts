f2e66ec883a1e02618c0eae491b4e49f6a522846
Mats Andresen <mats.andresen@gmx.de>
2023-02-06 12:56:10 +0100

    Use proper image upload directory and normalize path

--- src/routes/api/upload-image/+server.ts
+++ src/routes/api/upload-image/+server.ts
@@ -1,12 +1,17 @@
 import appCtx from '$lib/server/appCtx'
 import type { RequestEvent } from '@sveltejs/kit'
 import { writeFileSync } from 'fs'
+import { normalize } from 'path'
 
 export async function POST(event: RequestEvent) {
   try {
     const { config } = await appCtx()
     const payload: ImageUploadRequestPayload = JSON.parse(await event.request.text())
-    writeFileSync(config.imageDir + payload.fileName, payload.dataBase64Encoded, 'base64')
+    writeFileSync(
+      normalize(config.imageDir + payload.fileName),
+      payload.dataBase64Encoded,
+      'base64'
+    )
     return new Response(null, {
       status: 200
     })
